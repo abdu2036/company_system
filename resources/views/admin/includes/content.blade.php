cf0a4af794571e76b1c7564a8421a26f94d656d9
abdu2036 <abdsalaam1993@gmail.com>
2026-04-14 09:50:00 +0200

    تم التعديل في footbar للشريط السفلي

--- resources/views/admin/includes/content.blade.php
+++ resources/views/admin/includes/content.blade.php
@@ -1,27 +1,41 @@
- <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-12">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="{{ route('companies.index') }}">الرئيسية</a></li>
-              <li class="breadcrumb-item active">@yield('title')</li>
-            </ol>
-            
-          </div><!-- /.col -->
-          <div class="col-sm-12">
-            <h1 class="m-0 text-dark">@yield('title')</h1>
-            <br>
-            @yield('content')
-          </div><!-- /.col -->
+<div class="content-wrapper">
 
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
+    <!-- Content Header (العنوان + Breadcrumb) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+
+                <!-- Breadcrumb -->
+                <div class="col-sm-12">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item">
+                            <a href="{{ route('companies.index') }}">الرئيسية</a>
+                        </li>
+                        <li class="breadcrumb-item active">
+                            @yield('title')
+                        </li>
+                    </ol>
+                </div>
+
+                <!-- عنوان الصفحة -->
+                <div class="col-sm-12">
+                    <h1 class="m-0 text-dark">@yield('title')</h1>
+                </div>
+
+            </div>
+        </div>
     </div>
     <!-- /.content-header -->
 
-    <!-- Main content -->
- 
+
+    <!-- ✅ المحتوى الرئيسي (المهم جداً) -->
+    <section class="content">
+        <div class="container-fluid">
+
+            @yield('content')
+
+        </div>
+    </section>
     <!-- /.content -->
-  </div>
+
+</div>
